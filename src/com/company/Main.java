1ad34a51bd23385ce62b5429f86e6d8b7a117336
Jordan Begian <jordanbegian@Jordans-MacBook-Pro.local>
2019-01-23 10:41:20 -0500

    created project

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+//Create a new project in IntelliJ IDEA named ArrayPractice
+//Initialize your project as a Git repository and be sure to add and commit all changes as you work
+//Declare an array named dailyAverageTemp
+//Instruct the user to input daily average temperatures (in degrees Fahrenheit) for 7 days
+//Each time the user inputs a value, add it to your dailyAverageTemp array
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
