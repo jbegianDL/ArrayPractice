5b5d8dfbcbea743a3f7a7884fc27ab55bb424bad
Jordan Begian <jordanbegian@Jordans-MacBook-Pro.local>
2019-01-23 11:32:48 -0500

    added for loop/added switch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,14 @@
 //Each time the user inputs a value, add it to your dailyAverageTemp array
 
 package com.company;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner inputTemp = new Scanner();
+        Scanner inputTemp = new Scanner(System.in);
 	    double [] dailyAverageTemp = new double[7];
 	    String promptDay1 = "Please enter the average temperature for Monday";
         String promptDay2 = "Please enter the average temperature for Tuesday";
@@ -21,6 +23,44 @@ public class Main {
         String promptDay7 = "Please enter the average temperature for Sunday";
         int days = 7;
 
-        
+        for(int count = 1; count <= days; count++){
+            switch(count){
+                case 1:
+                    System.out.println(promptDay1);
+                    double usrInput1 = inputTemp.nextInt();
+                    dailyAverageTemp[0] = usrInput1;
+                    break;
+                case 2:
+                    System.out.println(promptDay2);
+                    double usrInput2 = inputTemp.nextInt();
+                    dailyAverageTemp[1] = usrInput2;
+                    break;
+                case 3:
+                    System.out.println(promptDay3);
+                    double usrInput3 = inputTemp.nextInt();
+                    dailyAverageTemp[2] = usrInput3;
+                    break;
+                case 4:
+                    System.out.println(promptDay4);
+                    double usrInput4 = inputTemp.nextInt();
+                    dailyAverageTemp[3] = usrInput4;
+                    break;
+                case 5:
+                    System.out.println(promptDay5);
+                    double usrInput5 = inputTemp.nextInt();
+                    dailyAverageTemp[4] = usrInput5;
+                    break;
+                case 6:
+                    System.out.println(promptDay6);
+                    double usrInput6 = inputTemp.nextInt();
+                    dailyAverageTemp[5] = usrInput6;
+                    break;
+                case 7:
+                    System.out.println(promptDay7);
+                    double usrInput7 = inputTemp.nextInt();
+                    dailyAverageTemp[6] = usrInput7;
+                    break;
+            }
+        }
     }
 }
